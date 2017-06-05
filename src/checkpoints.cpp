161e364e31fd83fa0e48d5b26ab4587995ba9ab1
spayseMickghee <Spayseghost@gmail.com>
2017-06-05 10:32:41 -0400

    checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -28,7 +28,7 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
        
-        ( 85,   uint256("0x0000001187ebcce34716bbf778da5346cbbba31c91ee5f71f60002a051dbbfd8") )
+      //  ( 85,   uint256("0x0000001187ebcce34716bbf778da5346cbbba31c91ee5f71f60002a051dbbfd8") )
 
 
     ;
